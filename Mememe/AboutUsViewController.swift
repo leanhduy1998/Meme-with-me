da6c1b399e6ac5401e2e289b0ea2085943ab6e92
Duy Le <leanhduy1998@yahoo.com>
2017-11-18 05:28:32 -0500

    update ui and add app icon

--- Mememe/AboutUsViewController.swift
+++ Mememe/AboutUsViewController.swift
@@ -13,6 +13,11 @@ class AboutUsViewController: UIViewController {
     @IBOutlet weak var iv1: UIImageView!
     @IBOutlet weak var iv2: UIImageView!
     
+    @IBAction func patreonBtnPressed(_ sender: Any) {
+        UIApplication.shared.openURL(NSURL(string: "https://www.patreon.com/taydrew")! as URL)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         iv1 = UIImageViewHelper.roundImageView(imageview: iv1, radius: 5)
